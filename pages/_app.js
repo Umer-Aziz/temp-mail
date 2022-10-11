cbb29a4f22bee47a1f161a53371c68721c0dda64
Umer-Aziz <umars25271997@gmail.com>
2022-10-11 20:47:52 +0500

    meta tag added

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,10 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="author" content="Umer Aziz" />
+        <meta name="description" content="Temporary mail - Keep your real mailbox clean and secure. Temp Mail provides temporary, secure, anonymous, free, disposable email address." />
+        <meta name="keywords" content="temp mail, temp-mail,Temporary mail , disposable mail " />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"/>
       </Head>
       <Navbar/>
       <Component {...pageProps}/>
--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"/>
       </Head>
       <body>
         <Main />
