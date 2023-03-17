4ec088dcb063ffbeaa8ba71fb759169914a93507
Umer Aziz <62507205+Umer-Aziz@users.noreply.github.com>
2023-03-17 19:20:52 +0500

    Update Navbar.js

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
     <>
     <nav className='border-md-bottom'>
       <div className="p-3 container d-flex justify-content-between align-items-center">
-      <p className="mt-2 h4 text-center font-boo temp-mail" style={{cursor:"pointer"}}><MdOutlineAlternateEmail className="mail-logo text-danger"/>TEMP<span className="text-danger border border-2 border-danger">MAIL</span></p>
+      <p className="mt-2 h4 text-center font-boo temp-mail text-white" style={{cursor:"pointer"}}><MdOutlineAlternateEmail className="mail-logo text-danger"/>TEMP<span className="text-danger border border-2 border-danger">MAIL</span></p>
     <div>
     <Link href="https://www.linkedin.com/in/umer-aziz-b13b841b2/"><a className="link-light me-4" target="_blank"><BsLinkedin className="fs-4 linkedin-icon"/></a></Link>
     <Link href="https://github.com/Umer-Aziz/temp-mail"><a className="link-light" target="_blank"><BsGithub className="fs-4 github-icon"/></a></Link>
